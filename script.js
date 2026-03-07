01643b597611a90f12c0ad319bb88156e251a19f
g-akca <gok.akca66@gmail.com>
2026-03-08 02:03:30 +0300

    Fix a bug, add button hover styles

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const questionBtns = document.querySelectorAll(".question-btn");
 const answerTexts = document.querySelectorAll(".answer-text");
+const icons = document.querySelectorAll(".question-btn img");
 
 questionBtns.forEach(btn => btn.addEventListener("click", () => {
     const targetAnswer = document.getElementById(btn.getAttribute("aria-controls"));
-    
+
     if (targetAnswer.hidden) {
         answerTexts.forEach(ans => ans.hidden = true);
+        icons.forEach(icon => icon.src = "images/icon-plus.svg");
         targetAnswer.hidden = false;
         btn.querySelector("img").src = "images/icon-minus.svg";
     }
